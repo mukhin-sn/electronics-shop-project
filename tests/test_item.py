f53ee8bdae023a23079cef8851645fc7019cebca
Sergey Mukhin <senikomu@gmail.com>
2023-10-18 01:53:25 +0700

    update test_item.py add method test_item_name add method test_instantiate_from_csv add method test_string_to_number

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,12 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
 from src.item import *
+
 # import pytest
 
 
 # @pytest.fixture()
 # def create_object():
 #     return Item("Name", 1250, 2)
+
+
+def test_item_name():
+    item = Item("Телефон", 10000, 5)
+    assert item.name == "Телефон"
+    item.name = 3 * "Bla"
+    assert item.name == "BlaBlaBla"
+    item.name = "Very" + 3 * "Bla"
+    assert item.name == "VeryBlaBla"
 
 
 def test_item_calculate_total_price():
@@ -21,3 +31,18 @@ def test_apply_discount():
     Item.pay_rate = 0.5
     item_1.apply_discount()
     assert item_1.price == 500
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv("../src/items.csv")
+    # assert len(Item.all) == 5
+    item_3 = Item.all[2]
+    assert item_3.name == "Кабель"
+    assert item_3.price == "10"
+    assert item_3.quantity == "5"
+
+
+def test_string_to_number():
+    assert Item.string_to_number("12") == 12
+    assert Item.string_to_number("10.0") == 10
+    assert Item.string_to_number("125.3") == 125
