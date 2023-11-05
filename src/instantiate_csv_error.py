25663674d5a19dffc06efd1f9625a08f64153e29
mukhin-sn <senikomu@gmail.com>
2023-11-05 23:21:59 +0700

    Creating an Exception Class

--- src/instantiate_csv_error.py
+++ src/instantiate_csv_error.py
@@ -1,12 +1,12 @@
 class InstantiateCSVError(Exception):
-    """
-    Общий класс исключений
-    """
-    def __init__(self, *args, **kwargs):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = 'Неизвестная ошибка'
+    """Класс - исключение"""
+    def __init__(self):
+        self.message = 'Файл поврежден'
 
-    def __str__(self):
-        return self.message
+
+class InstError:
+    def __init__(self, field_name):
+        if len(field_name) != 3:
+            raise InstantiateCSVError
+        else:
+            self.field_name = field_name
